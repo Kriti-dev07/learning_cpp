bb93fc0bb11414cdac93c39ad1f7b59ae99b872f
Kriti-dev07 <erkriti.mittal@gmail.com>
2025-07-18 03:57:00 +0530

    added file with 5 main data types

---
+++ BASICS/03_data_types.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<typeinfo>
+using namespace std;
+int main(){
+int i=42;
+float f=3.14f;    //f to store as float otherwise it would be stored as double
+double d= 2.23456 ;
+char c='x';
+bool b=true;
+string s= "C++ is a programming language ";
+
+cout << "int: " << i << " (type: " << typeid(i).name() << ")" << endl;
+    cout << "float: " << f << endl;
+    cout << "double: " << d << endl;
+    cout << "char: " << c << endl;
+    cout << "bool: " << b << endl;
+    cout << "string: " << s << endl;
+
+    return 0;
+}
+
+
+
+
+
