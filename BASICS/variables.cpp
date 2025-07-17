e6734ff72d7fa9db8812f2002e3bfbc7fe4082ca
Kriti-dev07 <erkriti.mittal@gmail.com>
2025-07-18 02:30:55 +0530

    added variables.cpp

---
+++ BASICS/variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    //defining variable values
+    int age=20;
+    float height=5.9;
+    char grade='A';
+    string name = "Kriti";
+bool is_student= true;
+//printing
+cout<< "name : " <<name<<endl;
+cout<< " age : " <<age<<endl;
+cout<< "grade : " <<grade<<endl;
+cout<< "is a student? : " <<is_student<<endl;
+return 0;}
